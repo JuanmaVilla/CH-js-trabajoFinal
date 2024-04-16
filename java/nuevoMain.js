9f87bc40d19f36f31eecc0ffc387c113abc59b8e
JuanmaVilla <juanmanuelvillavicenciovelasco@Juans-MacBook-Pro.local>
2024-04-15 21:27:51 -0300

    remplazando el http// por www. en el fetch (todavia no funciona)

--- java/nuevoMain.js
+++ java/nuevoMain.js
@@ -1,6 +1,6 @@
 async function obtenerEventos() {
     try {
-      const response = await fetch('https://localhost:3000/JSON/eventos.json'); // Reemplaza 'url_del_servidor/eventos' con la URL real de tu servidor que proporciona los datos de los eventos.
+      const response = await fetch('www.localhost:3000/JSON/eventos.json'); 
       debugger
       if (!response.ok) {
         throw new Error('No se pudo obtener la lista de eventos');
