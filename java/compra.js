7329364f75eac5e6661eaa2c525df5e03531a12c
JuanmaVilla <juanmanuelvillavicenciovelasco@Juans-MacBook-Pro.local>
2024-04-18 23:22:50 -0300

    compra implementada(falta)

--- java/compra.js
+++ java/compra.js
@@ -4,6 +4,8 @@ const evento_mostrado = [detalle_Evento];
 
 const seccion_Evento = document.querySelector("#evento_Compra");
 const botones_Compra = document.querySelector("#botones_Compra");
+const botones_Compra_abajo = document.querySelector("#boton_Comprar_abajo");
+const contenedor = document.querySelector("#contenedor");
 
 function cargar_Evento(){
     if(evento_mostrado && evento_mostrado.length > 0){
@@ -42,6 +44,74 @@ function cargar_Evento(){
             
         });
     }
+
+    cargar_Compra_Evento();
+    setTimeout(()=>{
+        acutualizarBotonComprar();
+    },1000)
+
 }
 
 cargar_Evento();
+
+let boton_desplegar = document.querySelectorAll(".boton_Comprar");
+
+function acutualizarBotonComprar(){
+    boton_desplegar= document.querySelectorAll(".boton_Comprar");
+
+    boton_desplegar.forEach(boton => {
+        boton.addEventListener("click", desplegar_Compra);
+    })
+
+}
+
+function desplegar_Compra(){
+    console.log("ESTOYYYY")
+        botones_Compra.classList.add("disabled");
+        contenedor.classList.remove("disabled");
+        botones_Compra_abajo.classList.remove("disabled");
+}
+
+function cargar_Compra_Evento(){
+    if(evento_mostrado && evento_mostrado.length > 0){
+        contenedor.innerHTML = '';
+    
+        evento_mostrado.forEach(evento => {
+            const div = document.createElement("div");
+            div.classList.add("Detalles_Compra");
+            div.innerHTML = `
+            <div class="Evento_Fav">
+            <img class="Imagen_Evento_Favoritos" src="${evento.imagen}" alt="${evento.titulo}">
+            <div class="Titulo_Evento_Favoritos">
+               <small>Titulo</small>
+               <h3>${evento.titulo}</h3>
+           </div>
+           <div class="Fecha_Evento_Favoritos">
+               <small>Desde el</small>
+               <h3>${evento.fecha}</h3>
+               <small>hasta el</small>
+               <h3>${evento.fecha}</h3>
+           </div>
+           <div class="Precio_Evento_Favoritos">
+               <small>Precio</small>
+               <h3>$${evento.precio}</h3>
+            </div>
+            <div class="Cantidad_Evento">
+                <small>Cantidad</small>
+                <h3>1</h3>
+            </div>
+           <button id="${evento.id}" class="Comprar_Mas_Evento"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>
+          </svg>
+          <button id="${evento.id}" class="Comprar_Mas_Evento"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-circle-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
+          </svg>
+           </button>
+        </div>  
+
+            `
+        contenedor.append(div);
+            
+        });
+    }
+}
